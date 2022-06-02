779d34eec8cbb9a8fe7bb38cfd44647f3eba0fa9
Pablo <pabloechegaray95@gmail.com>
2022-06-02 15:46:05 -0300

    Se agregan las vistas jsp

--- src/main/java/clinicacanina/controladores/AmbulanciaController.java
+++ src/main/java/clinicacanina/controladores/AmbulanciaController.java
@@ -1,19 +1,25 @@
 package clinicacanina.controladores;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import clinicacanina.modelo.Ambulancia;
 import clinicacanina.servicios.AmbulanciaService;
 
+@Controller
 public class AmbulanciaController {
 	
 	private AmbulanciaService ambulanciaService;
 	
+	@Autowired
 	public AmbulanciaController(AmbulanciaService ambulanciaService) {
 		this.ambulanciaService = ambulanciaService;
 	}
 
+	@RequestMapping(path="/pedir-ambulancia")
 	public ModelAndView pedirAmbulancia() {
 		ModelMap model = new ModelMap();
 		String viewName = "ambulancia-en-camino";
