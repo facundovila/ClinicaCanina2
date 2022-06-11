b99fbd8bbb5203eeb3197d34fae917082b178e64
Camila <camilabelen906@gmail.com>
2022-06-11 15:29:29 -0300

    agrego variable

--- src/main/java/clinicacanina/modelo/Medico.java
+++ src/main/java/clinicacanina/modelo/Medico.java
@@ -10,6 +10,7 @@ public class Medico {
 	private Long id;
 	
 	private String nombre;
+	private String apellido;
 	private Integer horarioEntrada; 
 	private Integer horarioSalida;
 	private Boolean disponibilidad;
